35fb1cf3735d59db1e490dc90480fe758060dfc5
dimka90 <dimkayilrit@gmail.com>
2026-02-04 20:22:01 +0100

    chore: full implemented of prepared

--- powertoken-go/internal/prepared.go
+++ powertoken-go/internal/prepared.go
@@ -1,6 +1,7 @@
 package internal
+import "fmt"
 type PrepaidMeter struct{
-MeterNumber string
+MeterNumber  string
 CustomerName string
 Address string
 Balance float64
@@ -19,4 +20,23 @@ Tariff: tariff,
 
 func (p *PrepaidMeter) GetMeterNumber() string {
 return p.MeterNumber
+}
+
+func (p *PrepaidMeter) GetMeterType() MeterType{
+return  Prepaid
+}
+
+func (p *PrepaidMeter) GetBalance() float64{
+return p.Balance
+}
+func (p *PrepaidMeter) Addunits(units float64) error{
+if units <= 0 {
+return fmt.Errorf("units must be greater than zero")
+}
+p.Balance += units
+return nil
+}
+
+func (p *PrepaidMeter) GetCustomerName()  string{
+return p.CustomerName
 }
